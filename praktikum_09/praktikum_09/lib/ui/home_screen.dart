fa8b4217c45c31bda49cb645cbc2fa0b37edc72d
luthfi2003 <21102148@ittelkom-pwt.ac.id>
2024-06-12 22:08:38 +0700

    firebase_auth_create-read-update-delete

--- praktikum_09/praktikum_09/lib/ui/home_screen.dart
+++ praktikum_09/praktikum_09/lib/ui/home_screen.dart
@@ -1,39 +1,91 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:praktikum_09/ui/login.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController noteController = TextEditingController();
+  @override
+  void dispose() {
+    titleController.dispose();
+    noteController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "HOME SCREEN",
-              style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff3D4DE0)),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                FirebaseAuth.instance
-                    .signOut()
-                    .then((value) => Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginScreen(),
-                        ),
-                        (route) => false));
-              },
-              child: const Text('Keluar'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Note'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              GoogleSignIn().signOut();
+              FirebaseAuth.instance
+                  .signOut()
+                  .then((value) => Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(),
+                      ),
+                      (route) => false));
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                            controller: titleController,
+                            decoration:
+                                const InputDecoration(hintText: 'Title')),
+                        const SizedBox(height: 10.0),
+                        SizedBox(
+                            height: 300,
+                            child: TextFormField(
+                                controller: noteController,
+                                maxLines: null, // Set this
+                                expands: true, // and this
+                                keyboardType: TextInputType.multiline,
+                                decoration: const InputDecoration(
+                                    hintText: 'Write a note', filled: true))),
+                        Padding(
+                            padding: EdgeInsets.only(
+                                bottom:
+                                    MediaQuery.of(context).viewInsets.bottom),
+                            child: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: const Text('Save'))))
+                      ],
+                    ),
+                  ),
+                );
+              });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
